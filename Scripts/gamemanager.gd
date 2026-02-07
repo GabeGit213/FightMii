10c1d2da94ea4cca1fd16aa1721c479a3849adc3
gabe <salvarilie@gmail.com>
2026-02-07 15:39:59 +0200

    separated export files from main files and started work on functionality, need to add 2 new people and replace valk

---
+++ Scripts/gamemanager.gd
@@ -0,0 +1,14 @@
+extends Node
+
+var selected_character: String = ""
+
+var character_data = {
+	"Gabe": {"weapon_type": "Catalyst", "ability": ""},
+	"Eden": {"weapon_type": "Ranged", "ability": ""},
+	"Skully": {"weapon_type": "Scythe", "ability": ""},
+	"Ravioli": {"weapon_type": "", "ability": ""},
+	"Stefi": {"weapon_type": "", "ability": ""},
+	"Ari": {"weapon_type": "", "ability": ""},
+	"Gabi": {"weapon_type": "", "ability": ""},
+	
+}
